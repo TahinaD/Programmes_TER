1e1fdff30b70f2c9de13a7edb23eed5c16e6eeae
Tahina <dombrowski.tahina47@gmail.com>
2026-06-17 10:47:31 +0200

    Fonctionnelle mise à jour

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 algo_base_wos.c est l'implantation de l'algorithme de base WOS sur un axe avec deux conditions limites de Robin.
 
 algo_var_fonct_wos.c est l'implantation à la fois de la variante à reinjections avec mémoire et de la fonctionnelle.
-Il faut d'abord exécuter la variante avec les paramètres choisis, puis relancer le programme sans paramètres.
+Il faut d'abord exécuter la variante avec les paramètres choisis, puis relancer le programme avec un paramètre h.
+Le seuil de rebonds doit être supérieur à la température T2, et assez grand pour que Mmax > 1.
 
 Les scripts script_vary_*_wos.sh prennent le programme algo_wos_1D_*.c et créent un graphe de variation de la valeur de la température estimée en fonction de la variation du paramètre correspondant.
 
--- algo_var_fonct_wos.c
+++ algo_var_fonct_wos.c
@@ -7,24 +7,22 @@
 
 int main(int argc, char* argv[]){
  
-  if (argc == 1) {
+  if (argc == 2) {
      FILE* fptr;
      fptr = fopen("informations_wos_1D.txt", "r");
      
      if (fptr == NULL) {
        printf("fail open file");
      } else {
-       printf("ouverture du fichier\n");
-       unsigned long nrealisations, nrebons = 0, cur_nreb;
+       unsigned long nrealisations, cur_nreb;
        int nbinter, Tcur = -1;
-       double sum = 0, sum2 = 0, proba;
+       double sum = 0, sum2 = 0, h = atof(argv[1]), proba;
        double estim, std, festim, fstd;
-
+      
        fscanf(fptr, "%ld %lf %d", &nrealisations, &proba, &nbinter);
-       printf("%ld %lf %d\n", nrealisations, proba, nbinter);
        double* inter = malloc(nbinter*sizeof(double));
+       proba = h/(h + 400);
 
-       printf("instanciation interfaces\n");
        for (int i = 0; i < nbinter; i++) {
          inter[i] = 0;
        }
@@ -37,15 +35,11 @@ int main(int argc, char* argv[]){
        double w = 0;
        fscanf(fptr, "%ld ", &cur_nreb);
        while (cur_nreb != -1) {
-         for (int reb = 0; reb < cur_nreb - 1; reb++) {
+         for (int reb = 0; reb < cur_nreb; reb++) {
            fscanf(fptr, "%d ", &Tcur);
-           w += pow((1-proba), nrebons)*proba*inter[Tcur];
-           nrebons++;
+           w += pow((1-proba), reb)*proba*inter[Tcur];
          }
          
-         fscanf(fptr, "%d \n", &Tcur);
-         w += pow((1-proba), nrebons)*inter[Tcur];
-         nrebons = 0;
          sum += w;
          sum2 += w*w;
          w = 0;
@@ -56,11 +50,10 @@ int main(int argc, char* argv[]){
        fscanf(fptr, "%lf %lf \n", &festim, &fstd);
        free(inter);
 
-       printf("fin de la lecture\n");
        estim = sum/nrealisations;
        std = sqrt((sum2/nrealisations - estim*estim)/nrealisations);
-       printf("%lf +- %lf\n", estim, std);
-       printf("fichier : %lf +- %lf\n", festim, fstd);
+       printf("proba : %lf, estim : %lf, std : %lf\n", proba, estim, std);
+       printf("fichier estim : %lf, fichier std : %lf\n", festim, fstd);
      }
 
      fclose(fptr);
@@ -83,7 +76,7 @@ int main(int argc, char* argv[]){
 
   double T1 = 20;
   double T2 = 50;
-  double seuil_rebons = atof(argv[4]);
+  double seuil_rebonds = atof(argv[4]);
 
   double borne_inf = 0;
   double borne_sup = 50;
@@ -92,10 +85,10 @@ int main(int argc, char* argv[]){
   double h = atof(argv[2]);
   double proba = h/(h + (lambda/delta));
   double x = atof(argv[3]);
-
+  
   double longueur = sqrt(pow((borne_sup - borne_inf),2));
   
-  double Tx = (x/((lambda/h)+longueur))*T2 + (1- (x/((lambda/h)+longueur)))*T1;
+  double Tx = x/longueur * (T1 + (T2 - T1)/((h*longueur/delta) + 2)) + (1 - x/longueur) * (T2 + (T1 - T2)/((h*longueur/delta) + 2)) ;
   srand(time(0));
 
   FILE* fptr;
@@ -107,22 +100,22 @@ int main(int argc, char* argv[]){
     return 0;
   }
 
-  unsigned long Mmax = log(T2/seuil_rebons)/log(1 - proba); 
+  unsigned long Mmax = log(T2/seuil_rebonds)/log(1 - proba); 
 
   fprintf(fptr, "%ld %lf 2 %lf %lf\n", nrealisations, proba, T1, T2);
   char ligne[2*Mmax + 2];
-  char str_rebons[(int)log10(Mmax) + 2];
+  char str_rebonds[(int)log10(Mmax) + 2];
   int hist[Mmax];
 
   for (unsigned long j = 0; j < Mmax; j++){
     hist[j] = -1;
   }
-
+  
   for (unsigned long i = 0; i < nrealisations; i++){
-    unsigned long nrebons = 0;
+    unsigned long nrebonds = 0;
     strcpy(ligne, " ");
     
-    while (nrebons < Mmax) {
+    while (nrebonds < Mmax) {
       dis_inf = x - borne_inf;
       dis_sup = borne_sup - x;
 
@@ -132,36 +125,32 @@ int main(int argc, char* argv[]){
       x += ((choice > RAND_MAX/2) ? sphere : -sphere);
 
       if (x <= borne_inf + epsilon) {
-        hist[nrebons] = 0;
-        nrebons++;
+        hist[nrebonds] = 0;
+        nrebonds++;
         x = x + delta;
         strcat(ligne,"0 ");
       }
     
       if (x >= borne_sup - epsilon) {
-        hist[nrebons] = 1;
-        nrebons++;
+        hist[nrebonds] = 1;
+        nrebonds++;
         x = x - delta;
         strcat(ligne, "1 ");
       }
     }
 
-    sprintf(str_rebons, "%ld", nrebons);
-    fputs(str_rebons, fptr);
+    sprintf(str_rebonds, "%ld", nrebonds);
+    fputs(str_rebonds, fptr);
     fputs(ligne, fptr);
     fprintf(fptr, "\n");
 
     int T;
-    double w = ((hist[nrebons - 1] == 0) ? T1 : T2);
-    for (unsigned long j = nrebons - 1; j > 0; j--){
+    double w = 0;
+    for (unsigned long j = 0; j < nrebonds; j++){
       T = ((hist[j] == 0) ? T1 : T2);
-      w = (1-proba)*w + proba*T;
+      w += pow((1-proba), j)*proba*T;
       hist[j] = -1;
     }
-     
-    T = ((hist[0] == 0) ? T1 : T2);
-    w = (1-proba)*w + proba*T;
-    hist[0] = -1;
 
     sum += w;
     sum2 += w*w;
@@ -173,8 +162,7 @@ int main(int argc, char* argv[]){
   std = sqrt((sum2/nrealisations - estim*estim)/nrealisations);
   fprintf(fptr, "-1 %lf %lf\n", estim, std);
   fclose(fptr);
-  printf("Le fichier d'informations a été rempli.\n");
 
-  printf("%lf = %lf +- %lf\n", Tx, estim, std);
+  printf("%lf = %lf +- %lf\n", estim, std, Tx);
   return 0;
 }
